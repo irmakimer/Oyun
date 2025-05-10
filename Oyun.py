13e44f6c7cde947fa2ec2376a87bbffa5d4ec555
Irmak İmer <159695615+irmakimer@users.noreply.github.com>
2025-05-10 20:09:56 +0300

    Ekran penceresi ve renkler oluşturuldu

--- Oyun.py
+++ Oyun.py
@@ -6,3 +6,15 @@ import time
 import os
 
 pygame.init()
+
+# Ekran
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Futbol Oyunu")
+clock = pygame.time.Clock()
+
+# Renkler
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 50, 50)
+GREEN = (0, 200, 0)
